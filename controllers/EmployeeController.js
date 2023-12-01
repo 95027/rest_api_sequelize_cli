03b0616db21f91b36546838ed34f93fa8c9e6e4c
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-12-01 14:40:19 +0530

    added update and delete methods

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -23,9 +23,88 @@ const addEmployeeDetails =async (req, res, next) => {
     }
 }
 
+// getting single employee
+const singleEmployee = async (req, res, next) => {
+
+    try {
+        const singleEmp = await Employee.findOne({
+            where : {
+                id : req.params.id
+            }
+        });
+
+        if(singleEmp){
+            res.json(singleEmp);
+        }
+        else{
+            res.json({message : 'employee not found'});
+        }
+        
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+// updating an employee
+const updatingEmployeeDetails = async (req, res, next) => {
+    try {
+        const emp = await Employee.findOne({
+            where : {
+                id : req.params.id
+            }
+        });
+
+        if(emp){
+            // const {name, email, gender, mobile} = req.body
+            await Employee.update({
+                name : req.body.name,
+                email : req.body.email,
+                gender : req.body.gender,
+                mobile : req.body.mobile,
+            }, {
+                where : {
+                    id : req.params.id
+                }
+            });
+
+            res.json({message : 'Employee details updated successfully'});
+        }
+        else{
+            res.json({message : 'employee not found'});
+        }
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+// delete an employee
+const deleteEmployee = async (req, res, next) => {
+    const emp = await Employee.findOne({
+        where : {
+            id : req.params.id
+        }
+    });
+
+    if(emp){
+        await Employee.destroy({
+            where : {
+                id : req.params.id
+            }
+        });
+        res.json({message : 'Employ details deleted successfully'});
+    }
+    else{
+        res.json({message : 'employee not found'});
+    }
+}
 
 
 module.exports = {
     getEmployeeDetails,
     addEmployeeDetails,
+    singleEmployee,
+    updatingEmployeeDetails,
+    deleteEmployee,
 }
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -5,6 +5,9 @@ const employeeController = require('../controllers/EmployeeController');
 
 router.post('/addEmployee', employeeController.addEmployeeDetails);
 router.get('/getEmployees', employeeController.getEmployeeDetails);
+router.get('/singleEmployee/:id', employeeController.singleEmployee); 
+router.put('/updateEmployee/:id', employeeController.updatingEmployeeDetails); 
+router.delete('/deleteEmployee/:id', employeeController.deleteEmployee); 
 
 
 module.exports = router;
