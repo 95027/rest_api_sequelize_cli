037255a1af4b4ca558802925db9ff63081efc0f4
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-12-01 10:17:55 +0530

    project setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({message : 'this is from api'})
+})
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
